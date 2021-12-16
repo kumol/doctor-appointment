177ea53c2afa44a26a3b2826232194cccdfd2f35
kumol <kumol01763@gmail.com>
2021-12-17 01:51:50 +0600

    profile view

--- src/container/Doctor/Doctor.js
+++ src/container/Doctor/Doctor.js
@@ -1,7 +1,7 @@
 // import { FormControl, InputBase, InputLabel, MenuItem, NativeSelect, Select, styled, TextField } from "@material-ui/core";
 // import { useState, useEffect } from "react";
 
-  
+import picture from "./../../assets/images/propic.jpg"
 
 const Doctor = (props) => {
     // const [city, setCity] = useState("");
@@ -29,12 +29,154 @@ const Doctor = (props) => {
 
                 </div>
             </div>
-            <div className="doctor-list">
+            <div className="doctor-list row bg-gray m-0">
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile">
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center",paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile">
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center", paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile"> 
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center",  paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile"> 
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center", paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile"> 
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center", paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile"> 
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center", paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile"> 
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center", paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile"> 
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center", paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile"> 
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center", paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-3" style={{paddingBottom: "24px"}}>
+                    <div className="profile"> 
+                        <div className="profilepicture">
+                            <img src={picture} style={{width:"100%", height:"200px", borderTopRightRadius: "5px", borderTopLeftRadius: "5px"}} alt="Logo" />
+                        </div>
+                        <div style={{textAlign:"center", paddingBottom:"10px", minHeight: "200px"}}>
+                            <div>Shishir Chokrobarti</div>
+                            <span>MBBS, FCBS ( Medicine )</span>
+                            <p>MAG Osmani Medical College</p>
+                            <button className="button-2">View Profile</button>
+                            <button className="button-1">Book Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row"> 
                 <button className="button-1">Button1</button>
                 <button className="button-2">Button2</button>
                 <button className="button-3">Button3</button>
                 <button className="button-4">Button4</button>
-            </div>
+            </div> 
         </div>
     )
 }
